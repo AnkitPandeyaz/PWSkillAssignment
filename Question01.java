6305c5c6159b1dab9d09b7016d2550f5fc6a5da5
Ankit Pandey <pandeyankit713@gmail.com>
2023-05-12 23:22:24 +0530

    first commit

---
+++ Question01.java
@@ -0,0 +1,12 @@
+/*Q1 - Take 2 integer values in two variables x and y and print their product.
+Sample Input : x=2, y=4 (Both integers)
+Sample Output : 8 */
+
+class Question01 {
+    public static void main(String[] args) {
+      int x = 2;
+      int y = 4;
+      int z = x * y;
+      System.out.println("Product of x*y  = " + z);
+    }
+  }
